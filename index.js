886b41f36a7bfcf87c43f6551e1c19e1fa279b38
Dylan <dylancarver14@gmail.com>
2022-07-19 15:24:02 -0500

    commit tree 063ac8d33d88d8ccb07ed7276fb75776435345d8

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+      return "This one is on me!"
+  } else if (feet > 400 && feet < 2000) {
+      return 'That will be twenty bucks.'
+  } else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+} else if (feet > 2500) {
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+  
+  
+  // WORKS BUT WRONG
+  // if (city === 'NYC') {
+  //     return 'Ok, sounds good.'
+  // } else if (city != 'NYC') {
+  //   return 'No go.'
+  // }
+  }
+ 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// const tip = 'generous';
+// let message
+function switchOnCharmFromTip(tip){
+
+  switch (tip) {
+        case 'generous':
+          return 'Thank you so much.';
+        case 'not as generous':
+          return 'Thank you.';
+        default:
+          return "Bye.";
+          
+  // if (tip === 'generous') {
+//     return 'Thank you so much.'
+// } else if (tip === 'not as generous') {
+//     return 'Thank you.'
+// } else if (tip != 'generous' || 'not as generous') {
+//     return 'Bye.'
+// }
+}
+//     
 }
